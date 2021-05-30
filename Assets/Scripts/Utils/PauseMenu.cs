b029270afe419df0f4092c1a80a710c3e1e99751
Surya Kashyap <surya.sk05@outlook.com>
2021-05-30 14:20:00 +0530

    Implement pause menu

---
+++ Assets/Scripts/Utils/PauseMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CultGame.Utils
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        public GameObject pauseMenu;
+        public GameObject otherUI;
+
+        public static bool isPaused = false;
+
+        private void Update()
+        {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Escape) || UnityEngine.Input.GetButtonDown("Esc"))
+            {
+                if (isPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        void Pause()
+        {
+            otherUI.SetActive(false);
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+            AudioListener.pause = true;
+            isPaused = true;
+        }
+
+        public void Resume()
+        {
+            otherUI.SetActive(true);
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+            AudioListener.pause = false;
+            isPaused = false;
+        }
+    }
+}
