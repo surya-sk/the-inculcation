80555ea2dfe96e1760ff0d1c1a28fc7d72478431
surya-sk <surya.sk05@outlook.com>
2021-12-05 18:33:32 -0400

    Place colliders

---
+++ Assets/Scripts/Gameplay/Ending.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CultGame.Player;
+
+namespace CultGame.Gameplay
+{
+    public class Ending : MonoBehaviour
+    {
+        public Canvas HintCanvas;
+        public ThirdPersonCharacterController Player;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            StartCoroutine(PromptJump());
+        }
+
+        IEnumerator PromptJump()
+        {
+            yield return null;
+        }
+    }
+}
