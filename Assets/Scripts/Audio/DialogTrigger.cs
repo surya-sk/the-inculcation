455b8c1d81ff14abc5c7566ce161aaaa919db9e3
surya-sk <surya.sk05@outlook.com>
2021-12-22 07:30:10 -0500

    Write DialogTrigger class

---
+++ Assets/Scripts/Audio/DialogTrigger.cs
@@ -0,0 +1,49 @@
+using CultGame.Saving;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CultGame.Audio
+{
+    public class DialogTrigger : MonoBehaviour, ISaveable
+    {
+        public AudioSource DialogAudioSource;
+        public AudioClip DialogClip;
+
+        bool m_Triggered;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_Triggered = false;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.tag == "Player")
+            {
+                StartCoroutine(PlayDialog());
+            }
+        }
+
+        IEnumerator PlayDialog()
+        {
+            m_Triggered = true;
+            DialogAudioSource.PlayOneShot(DialogClip);
+            GetComponent<BoxCollider>().enabled = !m_Triggered;
+            Debug.Log("Triggered");
+            yield return null;
+        }
+
+        public object CaptureState()
+        {
+            return m_Triggered;
+        }
+
+        public void RestoreState(object state)
+        {
+            m_Triggered = (bool)state;
+            GetComponent<BoxCollider>().enabled = !m_Triggered;
+        }
+    }
+}
