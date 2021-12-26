f41bb4d9203aa4f7433b29e44170a10382617c89
surya-sk <surya.sk05@outlook.com>
2021-12-26 09:27:32 -0500

    Fix formatting

--- Assets/Scripts/Gameplay/DayNight.cs
+++ Assets/Scripts/Gameplay/DayNight.cs
@@ -7,49 +7,49 @@ using CultGame.Saving;
 namespace CultGame.Gameplay
 {
     public class DayNight : MonoBehaviour, ISaveable
-{
-    public Light DirectionalLight;
-    public float IntensityMultiplier;
-    public Volume SkyFogVolume;
-    public VolumeProfile MorningVolume;
-    public VolumeProfile EveningVolume;
-    public VolumeProfile NightVolume;
+    {      
+        public Light DirectionalLight;
+        public float IntensityMultiplier;
+        public Volume SkyFogVolume;
+        public VolumeProfile MorningVolume;
+        public VolumeProfile EveningVolume;
+        public VolumeProfile NightVolume;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(ChangeTimeOfDay());   
-    }
-
-    IEnumerator ChangeTimeOfDay()
-    {
-        while(DirectionalLight.intensity > 0.4f)
+        // Start is called before the first frame update
+        void Start()
         {
-            DirectionalLight.intensity -= IntensityMultiplier;
+            StartCoroutine(ChangeTimeOfDay());   
+        }
+
+        IEnumerator ChangeTimeOfDay()
+        {
+            while(DirectionalLight.intensity > 0.4f)
+            {
+                DirectionalLight.intensity -= IntensityMultiplier;
+                UpdateSkybox();
+                yield return new WaitForSeconds(10f);
+            }
+        }
+
+        private void UpdateSkybox()
+        {
+            if (DirectionalLight.intensity > 2)
+                SkyFogVolume.profile = MorningVolume;
+            else if (DirectionalLight.intensity < 1.7 && DirectionalLight.intensity > 0.8)
+                SkyFogVolume.profile = EveningVolume;
+            else
+                SkyFogVolume.profile = NightVolume;
+        }
+
+        public object CaptureState()
+        {
+            return DirectionalLight.intensity;
+        }
+
+        public void RestoreState(object state)
+        {
+            DirectionalLight.intensity = (float)state;
             UpdateSkybox();
-            yield return new WaitForSeconds(10f);
         }
     }
-
-    private void UpdateSkybox()
-    {
-        if (DirectionalLight.intensity > 2)
-            SkyFogVolume.profile = MorningVolume;
-        else if (DirectionalLight.intensity < 1.7 && DirectionalLight.intensity > 0.8)
-            SkyFogVolume.profile = EveningVolume;
-        else
-            SkyFogVolume.profile = NightVolume;
-    }
-
-    public object CaptureState()
-    {
-        return DirectionalLight.intensity;
-    }
-
-    public void RestoreState(object state)
-    {
-        DirectionalLight.intensity = (float)state;
-        UpdateSkybox();
-    }
-}
 }
