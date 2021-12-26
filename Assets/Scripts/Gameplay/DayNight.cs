2b50f75f4fe8f77a33349604a6ef820892efdfbb
surya-sk <surya.sk05@outlook.com>
2021-12-26 09:23:55 -0500

    Implement day-night cycle

---
+++ Assets/Scripts/Gameplay/DayNight.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using CultGame.Saving;
+
+namespace CultGame.Gameplay
+{
+    public class DayNight : MonoBehaviour, ISaveable
+{
+    public Light DirectionalLight;
+    public float IntensityMultiplier;
+    public Volume SkyFogVolume;
+    public VolumeProfile MorningVolume;
+    public VolumeProfile EveningVolume;
+    public VolumeProfile NightVolume;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ChangeTimeOfDay());   
+    }
+
+    IEnumerator ChangeTimeOfDay()
+    {
+        while(DirectionalLight.intensity > 0.4f)
+        {
+            DirectionalLight.intensity -= IntensityMultiplier;
+            UpdateSkybox();
+            yield return new WaitForSeconds(10f);
+        }
+    }
+
+    private void UpdateSkybox()
+    {
+        if (DirectionalLight.intensity > 2)
+            SkyFogVolume.profile = MorningVolume;
+        else if (DirectionalLight.intensity < 1.7 && DirectionalLight.intensity > 0.8)
+            SkyFogVolume.profile = EveningVolume;
+        else
+            SkyFogVolume.profile = NightVolume;
+    }
+
+    public object CaptureState()
+    {
+        return DirectionalLight.intensity;
+    }
+
+    public void RestoreState(object state)
+    {
+        DirectionalLight.intensity = (float)state;
+    }
+}
+}
