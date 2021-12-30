e29765fa710727715e930df87ed30140d6e33844
surya-sk <surya.sk05@outlook.com>
2021-12-30 08:35:48 -0500

    Improve day night script

--- Assets/Scripts/Gameplay/DayNight.cs
+++ Assets/Scripts/Gameplay/DayNight.cs
@@ -8,16 +8,18 @@ namespace CultGame.Gameplay
 {
     public class DayNight : MonoBehaviour, ISaveable
     {      
-        public Light DirectionalLight;
         public float IntensityMultiplier;
         public Volume SkyFogVolume;
         public VolumeProfile MorningVolume;
         public VolumeProfile EveningVolume;
         public VolumeProfile NightVolume;
+        
+        private Light m_DirectionalLight;
     
         // Start is called before the first frame update
         void Start()
         {
+            m_DirectionalLight = GetComponent<Light>();
             StartCoroutine(ChangeTimeOfDay());   
         }
 
@@ -27,10 +29,10 @@ namespace CultGame.Gameplay
         /// <returns></returns>
         IEnumerator ChangeTimeOfDay()
         {
-            while(DirectionalLight.intensity > 0.4f)
+            while(m_DirectionalLight.intensity > 0.4f)
             {
-                DirectionalLight.intensity -= IntensityMultiplier;
-                Debug.Log(DirectionalLight.intensity);
+                m_DirectionalLight.intensity -= IntensityMultiplier;
+                Debug.Log(m_DirectionalLight.intensity);
                 UpdateSkybox();
                 yield return new WaitForSeconds(40f);
             }
@@ -41,9 +43,9 @@ namespace CultGame.Gameplay
         /// </summary>
         private void UpdateSkybox()
         {
-            if (DirectionalLight.intensity > 1.7)
+            if (m_DirectionalLight.intensity > 1.7)
                 SkyFogVolume.profile = MorningVolume;
-            else if (DirectionalLight.intensity < 1.7 && DirectionalLight.intensity > 0.8)
+            else if (m_DirectionalLight.intensity < 1.7 && m_DirectionalLight.intensity > 0.8)
                 SkyFogVolume.profile = EveningVolume;
             else
                 SkyFogVolume.profile = NightVolume;
@@ -51,12 +53,16 @@ namespace CultGame.Gameplay
 
         public object CaptureState()
         {
-            return DirectionalLight.intensity;
+            return m_DirectionalLight.intensity;
         }
 
         public void RestoreState(object state)
         {
-            DirectionalLight.intensity = (float)state;
+            if(m_DirectionalLight == null)
+            {
+                m_DirectionalLight = GetComponent<Light>();
+            }
+            m_DirectionalLight.intensity = (float)state;
             UpdateSkybox();
         }
     }
