82e8b6184b07bd0f45edc4fcb19666e26534ac82
surya-sk <surya.sk05@outlook.com>
2021-12-30 10:03:15 -0500

    Create enums to hold scene indexes

---
+++ Assets/Scripts/Utils/Scenes.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CultGame.Utils
+{
+    public enum Scenes
+    {
+        DISCLAIMER = 0,
+        MAIN_MENU = 1,
+        OPENING_TEXT = 2,
+        TITLE = 3,
+        SCENE_1 = 4,
+        SCENE_2 = 5, 
+        SCENE_3 = 6,
+        SCENE_4 = 7,
+        GAME_OVER = 8,
+        CREDITS = 9
+    }
+}
